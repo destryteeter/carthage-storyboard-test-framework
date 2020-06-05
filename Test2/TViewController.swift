2ce095f46bbe26772a7694140016b3c909d5c82b
Destry Teeter <destry.teeter@gmail.com>
2020-06-05 09:37:00 -0700

    > Initial commit

---
+++ Test2/TViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TViewController.swift
+//  Test2
+//
+//  Created by Destry Teeter on 6/5/20.
+//  Copyright © 2020 peoplepowerco. All rights reserved.
+//
+
+import UIKit
+
+class TViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Test")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
