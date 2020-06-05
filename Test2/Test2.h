a6e3682913a7527073cdef3a82be417f92749915
Destry Teeter <destry.teeter@gmail.com>
2020-06-05 08:58:33 -0700

    Initial Commit

---
+++ Test2/Test2.h
@@ -0,0 +1,19 @@
+//
+//  Test2.h
+//  Test2
+//
+//  Created by Destry Teeter on 6/5/20.
+//  Copyright © 2020 peoplepowerco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Test2.
+FOUNDATION_EXPORT double Test2VersionNumber;
+
+//! Project version string for Test2.
+FOUNDATION_EXPORT const unsigned char Test2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Test2/PublicHeader.h>
+
+
